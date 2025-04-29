8ed9b88ca25a37365529496e0a0052c20a9a515d
Victor Vega Martinez <victor.vega.martinez09@gmail.com>
2025-04-29 19:49:51 +0200

    Se han añadido diccionarios de  traduccion de los nombres de las strings de los optimizadores y las funciones de activacion. También se han añadido parametros opcionales en la clase modelo y se han quitado parametros obligatorios de train_and_evaluate.

--- entornoConda/entrenamiento.py
+++ entornoConda/entrenamiento.py
@@ -2,7 +2,7 @@ from Model import Model
 from funciones_preprocesamiento import preprocess_data
 
 
-def train_and_evaluate(dataset, num_batches, log_dir, batch_size,nombre_fichero_info_dataset):
+def train_and_evaluate(dataset,nombre_fichero_info_dataset, num_batches= 50, log_dir = "logs/fit/default_dataset_name", batch_size= 16):
     X_train_scaled, X_test_scaled, y_train_encoded, y_test_encoded = preprocess_data(dataset,nombre_fichero_info_dataset)
 
     model = Model(X_train_scaled, y_train_encoded,log_dir,batch_size,num_batches)
