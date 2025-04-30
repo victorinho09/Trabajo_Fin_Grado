533a1a955f7c8fc55b0cca94c030c267cfa894f0
Victor Vega Martinez <victor.vega.martinez09@gmail.com>
2025-04-30 17:04:12 +0200

    cambios

--- entornoConda/Model.py
+++ entornoConda/Model.py
@@ -64,14 +64,14 @@ class  Model():
 
         #Traduccion de las funciones de activacion disponibles por tf.keras
         self.available_hidden_activation_functions = {
-            "celu": tf.keras.activations.celu,
-            "glu": tf.keras.activations.glu,
-            "hard_shrink": tf.keras.activations.hard_shrink,
-            "hard_sigmoid": tf.keras.activations.hard_sigmoid,
+            #"celu": tf.keras.activations.celu, NO HAY SOPORTE DESDE TF.KERAS, solo desde keras. ¿tf.nn.ceu? -> no me la detecta
+            #"glu": tf.keras.activations.glu,
+            #"hard_shrink": tf.keras.activations.hard_shrink,
+            #"hard_sigmoid": tf.keras.activations.hard_sigmoid,
             "hard_silu": tf.keras.activations.hard_silu,
-            "hard_tanh": tf.keras.activations.hard_tanh,
+            #"hard_tanh": tf.keras.activations.hard_tanh,
             "leaky_relu": tf.keras.activations.leaky_relu,
-            "log_sigmoid": tf.keras.activations.log_sigmoid,
+            #"log_sigmoid": tf.keras.activations.log_sigmoid,
             "log_softmax": tf.keras.activations.log_softmax,
             "mish": tf.keras.activations.mish,
             "relu6": tf.keras.activations.relu6,
@@ -80,7 +80,7 @@ class  Model():
             "softmax": tf.keras.activations.softmax,
             "softplus": tf.keras.activations.softplus,
             "softsign": tf.keras.activations.softsign,
-            "soft_shrink": tf.keras.activations.soft_shrink,
+            #"soft_shrink": tf.keras.activations.soft_shrink,
             "tanh": tf.keras.activations.tanh,
             "selu": tf.keras.activations.selu,
             "elu": tf.keras.activations.elu,
@@ -89,11 +89,11 @@ class  Model():
             "gelu": tf.keras.activations.gelu,
             "silu": tf.keras.activations.silu,
             "swish": tf.keras.activations.silu,
-            "sparse_plus": tf.keras.activations.sparse_plus,
-            "sparsemax": tf.keras.activations.sparsemax,
-            "squareplus": tf.keras.activations.squareplus,
-            "tanh_shrink": tf.keras.activations.tanh_shrink,
-            "threshold": tf.keras.activations.threshold
+            #"sparse_plus": tf.keras.activations.sparse_plus,
+            #"sparsemax": tf.keras.activations.sparsemax,
+            #"squareplus": tf.keras.activations.squareplus,
+            #"tanh_shrink": tf.keras.activations.tanh_shrink,
+            #"threshold": tf.keras.activations.threshold
         }
 
 
@@ -108,8 +108,8 @@ class  Model():
         else:
             self.optimizers_list = ['adam', 'nadam', 'rmsprop','sgd']
 
-        self.optimizer = None
-        self.optimizer_name = 'adam'
+        self.optimizer = None #NO se hace en constructor porque falta lr
+        self.optimizer_name = self.optimizers_list[0] #Porque de momento se coge el primero para hacer pruebas
         self.optimizer_beta1= None
         self.optimizer_beta2 = None
         self.optimizer_rho = None
@@ -140,7 +140,7 @@ class  Model():
         self.max_lr = 1e-1
 
         # atributos de parametros pasados al tuner
-        self.max_trials = 10
+        self.max_trials = 15
         self.max_trials_activation_function_tuner = len(self.hidden_activation_function_list)
         self.objective = "val_accuracy"
         self.overwrite = True
@@ -210,7 +210,6 @@ class  Model():
         # deja en los atributos de la clase los resultados del fine tuning
         self.search()
 
-
         # asignamos resultados:
         self.assign_lr_to_model()
         self.assign_optimizer_to_model()
@@ -240,15 +239,8 @@ class  Model():
         print(f"Número de hidden layers óptimo: {self.best_hyperparameters['num_hidden']}")
 
     def assign_hidden_activation_function_to_model(self):
-        if self.best_hyperparameters['hidden_activation_function'] == 'relu':
-            self.hidden_activation_function = tf.keras.activations.relu
-        if self.best_hyperparameters['hidden_activation_function'] == "leaky_relu":
-            self.hidden_activation_function = tf.keras.activations.leaky_relu
-        if self.best_hyperparameters['hidden_activation_function'] == "elu":
-            self.hidden_activation_function = tf.keras.activations.elu
-        if self.best_hyperparameters['hidden_activation_function'] == "silu":
-            self.hidden_activation_function = tf.keras.activations.silu
-
+        # Se traduce la funcion de activacion de string -> funcion de tf.keras
+        self.hidden_activation_function = self.available_hidden_activation_functions[self.best_hyperparameters['hidden_activation_function']]
         print(f"Función de activación óptima: {self.best_hyperparameters['hidden_activation_function']}")
 
     def assign_lr_to_model(self):
@@ -256,19 +248,9 @@ class  Model():
         print(f"Tasa de aprendizaje óptima: {self.best_hyperparameters['lr']}")
 
     def assign_optimizer_to_model(self):
-        if self.best_hyperparameters['optimizer'] == 'adam':
-            self.optimizer_name = 'adam'
-            self.optimizer = tf.keras.optimizers.Adam(learning_rate=self.lr)
-        if self.best_hyperparameters['optimizer'] == "rmsprop":
-            self.optimizer_name = 'rmsprop'
-            self.optimizer = tf.keras.optimizers.RMSprop(learning_rate=self.lr)
-        if self.best_hyperparameters['optimizer'] == "nadam":
-            self.optimizer_name = 'nadam'
-            self.optimizer = tf.keras.optimizers.Nadam(learning_rate=self.lr)
-        if self.best_hyperparameters['optimizer'] == "sgd":
-            self.optimizer_name = 'sgd'
-            self.optimizer = tf.keras.optimizers.SGD(learning_rate=self.lr)
-
+        # Se traduce el optimizador de string -> funcion de tf.keras
+        #self.optimizer = self.available_optimizers[self.best_hyperparameters['optimizer']] No hace falta inicializarlo, ya que se hara ahora despues en la quinta vuelta
+        self.optimizer_name = self.best_hyperparameters['optimizer']
         print(f"Optimizador óptimo: {self.best_hyperparameters['optimizer']}")
 
     def assign_optimizer_with_params_to_model(self):
@@ -353,17 +335,11 @@ class  Model():
     def select_activation_function(self,hp):
         hidden_activation_function_choice = hp.Choice("hidden_activation_function", self.hidden_activation_function_list)
 
-        ##nuevo optimizer. Será un Adam, ya que la eleccion de optimizador se hace en la siguiente vuelta
+        ##nuevo optimizer. Será un Adam, ya que la eleccion de optimizador se hace en la siguiente vuelta. #Se traduce la funcion de activacion de string -> funcion de tf.keras
         self.optimizer = self.available_optimizers[self.optimizers_list[0]](learning_rate=self.lr)
 
-        if hidden_activation_function_choice == 'relu':
-            self.hidden_activation_function = tf.keras.activations.relu
-        if hidden_activation_function_choice == "leaky_relu":
-            self.hidden_activation_function = tf.keras.activations.leaky_relu
-        if hidden_activation_function_choice == "elu":
-            self.hidden_activation_function = tf.keras.activations.elu
-        if hidden_activation_function_choice == "silu":
-            self.hidden_activation_function = tf.keras.activations.silu
+        #Se traduce la funcion de activacion de string -> funcion de tf.keras
+        self.hidden_activation_function = self.available_hidden_activation_functions[hidden_activation_function_choice]
 
         model = self.create_and_compile_model()
         return model
@@ -375,21 +351,15 @@ class  Model():
         #también se reentrena lr, ya que salia aviso de que si se exploraban mas valores (menores de 1e-5) podia ir mejor
         lr = hp.Float("lr",min_value= (self.lr / 100), max_value= self.lr*100, sampling='log')
 
-        ###CUIDADO ---> SI LA LISTA CONTIENE ALGUNO QUE NO SEA ESTOS, SALTARÁ ERROR
-        if optimizer_choice == 'adam':
-            self.optimizer = tf.keras.optimizers.Adam(learning_rate=lr)
-        if optimizer_choice == "rmsprop":
-            self.optimizer = tf.keras.optimizers.RMSprop(learning_rate=lr)
-        if optimizer_choice == "nadam":
-            self.optimizer = tf.keras.optimizers.Nadam(learning_rate=lr)
-        if optimizer_choice == "sgd":
-            self.optimizer = tf.keras.optimizers.SGD(learning_rate=lr)
+        # Se traduce el optimizador de string -> funcion de tf.keras
+        self.optimizer = self.available_optimizers[optimizer_choice](learning_rate= lr)
 
         model = self.create_and_compile_model()
         return model
 
     #se deciden numero num_neuronas_por_capa y lr otra vez
     def select_num_neurons_per_hidden(self,hp):
+        # Se traduce el optimizador de string -> funcion de tf.keras
         self.optimizer = self.available_optimizers[self.optimizers_list[0]](learning_rate=self.lr)
 
         if self.X_train.shape[1] < self.min_num_neurons_per_hidden:
@@ -410,13 +380,14 @@ class  Model():
         self.num_hidden_layers = hp.Int("num_hidden", min_value=self.min_num_hidden_layers, max_value=self.max_num_hidden_layers)
 
         self.lr = hp.Float("lr", min_value=self.min_lr, max_value=self.max_lr, sampling='log')
+        # Se traduce el optimizador de string -> funcion de tf.keras
         self.optimizer = self.available_optimizers[self.optimizers_list[0]](learning_rate=self.lr) #No se inicializa en el constructor, ya que nos hace falta primero el valor de lr. Se coge el primer optimizador de la lista
 
         model = self.create_and_compile_model()
         return model
 
     def select_optimizer_params(self,hp):
-        nesterov_choice = np.float32(hp.Boolean("nesterov", default=True))
+        nesterov_choice = hp.Boolean("nesterov", default=True)
         beta1_choice = np.float32(hp.Float("beta1",min_value=0.7, max_value=0.99))
         beta2_choice = np.float32(hp.Float("beta2",min_value=0.85, max_value=0.9999))
 
@@ -424,17 +395,18 @@ class  Model():
             self.optimizer = tf.keras.optimizers.Adam(learning_rate = self.lr,beta_1=beta1_choice,beta_2=beta2_choice)
 
         if self.optimizer_name == 'rmsprop':
-            rho_choice= hp.Float("rho",min_value=0.7,max_value=0.95)
-            momentum_choice= hp.Float("momentum",min_value=0.0,max_value=0.9)
+            rho_choice= np.float32(hp.Float("rho",min_value=0.7,max_value=0.95))
+            momentum_choice= np.float32(hp.Float("momentum",min_value=0.0,max_value=0.9))
             self.optimizer = tf.keras.optimizers.RMSprop(learning_rate=self.lr,rho=rho_choice,momentum=momentum_choice)
 
         if self.optimizer_name == 'nadam':
             self.optimizer = tf.keras.optimizers.Nadam(learning_rate=self.lr,beta_1=beta1_choice,beta_2=beta2_choice)
 
         if self.optimizer_name == 'sgd':
-            momentum_choice = hp.Float("momentum",min_value=0.7, max_value=0.95)
+            momentum_choice = np.float32(hp.Float("momentum",min_value=0.7, max_value=0.95))
             self.optimizer = tf.keras.optimizers.SGD(learning_rate=self.lr,momentum=momentum_choice,nesterov=nesterov_choice)
 
+        #if self.optimizer_name == ''
         model = self.create_and_compile_model()
         return model
 
