dc8056e5d6c551d14bccc63eeccb5f26a55fa1ea
Victor Vega Martinez <victor.vega.martinez09@gmail.com>
2025-05-04 11:32:45 +0200

    cambios

--- entornoConda/entrenamiento.py
+++ entornoConda/entrenamiento.py
@@ -5,7 +5,7 @@ from funciones_preprocesamiento import preprocess_data
 def train_and_evaluate(dataset,nombre_fichero_info_dataset, num_batches= 50, log_dir = "logs/fit/default_dataset_name", batch_size= 16):
     X_train_scaled, X_test_scaled, y_train_encoded, y_test_encoded = preprocess_data(dataset,nombre_fichero_info_dataset)
 
-    model = Model(X_train_scaled, y_train_encoded,log_dir,batch_size,num_batches,user_min_num_neurons_per_hidden=30,user_max_num_neurons_per_hidden=20)
+    model = Model(X_train_scaled, y_train_encoded,log_dir,batch_size,num_batches)
     model.autotune()
     model.train()
     model.evaluate(X_test_scaled, y_test_encoded)
